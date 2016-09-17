05230f50c5673d03dc0b46048e1c8fe06c1938e5
Gina Yuan <gyuan@mit.edu>
2016-09-17 15:21:06 -0400

    restart django app

--- CareerTinderServer/CareerTinder/models.py
+++ CareerTinderServer/CareerTinder/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
-# Create your models here.
-class Student(models.Model):
-	name = models.CharField(max_length=200)
-	date_of_birth = models.DateTimeField('date of birth')
-	face_picture = models.FileField('face')
+
+class Hiree(models.Model):
+    name = models.CharField(max_length=200)
+    date_of_birth = models.DateTimeField('date of birth')
+    face_picture = models.FileField('face')
+    resume_picture = models.FileField('resume')
+
 
 class Company(models.Model):
-	name = models.CharField(max_length=200)
-	logo = models.FileField('company logo')
+    name = models.CharField(max_length=200)
+    logo = models.FileField('company logo')
+
+
+class Recruiter(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
--- CareerTinderServer/CareerTinder/views.py
+++ CareerTinderServer/CareerTinder/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, world. You're at the careertinder index.")
+    return HttpResponse("You're at careertinder index")
