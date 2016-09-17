7b2fdfd5458174e37bd1dc3e6421a5a46f5e9829
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2016-09-17 13:47:27 -0400

    Started creating database

--- CareerTinderServer/CareerTinder/models.py
+++ CareerTinderServer/CareerTinder/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+	name = models.CharField(max_length=200)
+	date_of_birth = models.DateTimeField('date of birth')
+	face_picture = models.FileField('face')
+
+class Company(models.Model):
+	name = models.CharField(max_length=200)
+	logo = models.FileField('company logo')
