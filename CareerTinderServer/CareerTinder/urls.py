719e808e9a44d1fe0eed76b15a3cdb66a683ef86
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2016-09-17 19:51:51 -0400

    Added urls

--- CareerTinderServer/CareerTinder/urls.py
+++ CareerTinderServer/CareerTinder/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 
-from . import views, tinder
+from . import views, tinder, resume_upload, resume_retrieval, take_picture
 
 urlpatterns = [
 	url(r'^review', tinder.index, name='index'),
+	url(r'^upload', resume_upload.index, name='index'),
+	url(r'^retrieve', resume_retrieval.index, name='index'),
+	url(r'^picture', take_picture.index, name='index'),
     url(r'^$', views.index, name='index')
 ]
