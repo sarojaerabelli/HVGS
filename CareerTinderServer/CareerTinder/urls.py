421d9614381374e01c532afbb7d267f729c4c722
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2016-09-17 19:49:22 -0400

    Created templates

--- CareerTinderServer/CareerTinder/urls.py
+++ CareerTinderServer/CareerTinder/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from . import views
+from . import views, tinder
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+	url(r'^review', tinder.index, name='index'),
+    url(r'^$', views.index, name='index')
 ]
