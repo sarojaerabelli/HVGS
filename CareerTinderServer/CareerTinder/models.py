791452ccf390cc293b647017430a8878bd187740
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2016-09-17 20:54:58 -0400

    With my changes

--- CareerTinderServer/CareerTinder/models.py
+++ CareerTinderServer/CareerTinder/models.py
@@ -24,6 +24,7 @@ class Company(models.Model):
 class Recruiter(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+    interested_hirees = models.ForeignKey(Hiree, default=[])
 
     def __str__(self):
         return "%s from %s" % (self.name, self.company)
--- CareerTinderServer/CareerTinder/tinder.py
+++ CareerTinderServer/CareerTinder/tinder.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from models import *
 
 # Create your views here.
 def index(request):
     template = loader.get_template('CareerTinder/review.html')
+    
+    recruiter = Recruiter.objects.get(id=1)
+    
     context = {
-        'name': 'John Doe'
+        'name': recruiter.name,
+        'photo_urls': [hiree.face_picture for hiree in recruiter.interested_hirees]
     }
     return HttpResponse(template.render(context, request))
