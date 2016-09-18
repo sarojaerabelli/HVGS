783b6ea78e870675ed40ad1b593ef4a0895c4214
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2016-09-17 23:57:40 -0400

    Added button form for yes/no

--- CareerTinderServer/CareerTinder/urls.py
+++ CareerTinderServer/CareerTinder/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 
 from . import views, tinder, resume_upload, resume_retrieval, take_picture
 
-app_name = 'CareerTinder'
 urlpatterns = [
-    url(r'^review', tinder.index, name='index'),
+    url(r'^review/(?P<idx>[0-9]+)/$', tinder.review_page, name='review_page'),
+    url(r'^review', tinder.review_page, name='review_page'),
     url(r'^upload', resume_upload.list, name='list'),
     url(r'^retrieve', resume_retrieval.index, name='index'),
     url(r'^picture', take_picture.index, name='index'),
