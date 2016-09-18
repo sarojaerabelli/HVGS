bd775ee4bca05b2ce1218fa398d36aaa520636a9
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2016-09-18 04:30:40 -0400

    Spruced up stuff

--- CareerTinderServer/CareerTinder/views.py
+++ CareerTinderServer/CareerTinder/views.py
@@ -7,3 +7,10 @@ def index(request):
     return render(request,
         'CareerTinder/index.html'
     )
+
+def error404(request):
+    template = loader.get_template('CareerTinder/no_page.html')
+    context = {
+        'message': "Oops, looks like this HackMIT team needed the 404 page after all."
+    }
+    return HttpResponse(template.render(context, request))
