e44fd084566aa56163f86261674ece2f785bc27c
Gina Yuan <gyuan@mit.edu>
2016-09-17 21:44:36 -0400

    finish resume_upload backend

--- CareerTinderServer/CareerTinder/forms.py
+++ CareerTinderServer/CareerTinder/forms.py
@@ -2,8 +2,8 @@
 from django import forms
 
 
-class DocumentForm(forms.Form):
-    docfile = forms.FileField(
+class ResumeForm(forms.Form):
+    resume_picture = forms.FileField(
         label='Select a file',
         help_text='max. 42 megabytes'
     )
